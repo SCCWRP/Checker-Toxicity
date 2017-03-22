0166e0c17648ea6f2ada1aef6d2bfa144fa651f8
Paul Smith <pauls@sccwrp.org>
2017-03-22 14:28:30 -0700

    new toxicity results check for reference toxicant data

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 import xlrd
 #from ordereddict import OrderedDict
 import collections
 import math
+
+## COMMON FUNCTIONS
+def dcAddErrorToList(error_column, row, error_to_add,df):
+	df.ix[int(row), 'row'] = str(row)
+	if error_column in df.columns:
+		# check if cell value is empty (nan) 
+		if(pd.isnull(df.ix[int(row), error_column])):
+			# no data exists in cell so add error
+	      		df.ix[int(row), error_column] = error_to_add
+		else:
+			# a previous error was recorded so append to it
+			# even though there may be data lets check to make sure it is not empty
+			if str(df.ix[int(row), error_column]):
+				print("There is already a previous error recorded: %s" % str(df.ix[int(row), error_column]))
+				df.ix[int(row), error_column] = str(df.ix[int(row), error_column]) + "," + error_to_add
+			else:
+				print("No error is recorded: %s" % str(df.ix[int(row), error_column]))
+	      			df.ix[int(row), error_column] = error_to_add
+	else:
+		df.ix[int(row), error_column] = error_to_add
+	return df
+
 # place the bight13 toxicity data in a location that the application can access
 #df = pd.ExcelFile('/Users/pauls/Documents/Projects/Bight18/Training/clean.xlsx')
 df = pd.ExcelFile('./clean.xlsx')
@@ -22,9 +45,20 @@ for tab in df_tab_names:
     	tab = df.parse(tab)
     	### and put into dictionary object
     	all_dataframes[count] = tab
+	### create tmp_row for tracking row numbers
+	all_dataframes[count]['tmp_row'] = all_dataframes[count].index
 	count = count + 1
 
-## SUMMARY RESULT CALCULATIONS
+## OTHER TOXICITY CHECKS
+
+# a reference toxicant in the matrix field must have data in the concentration field...cant be -88
+reftox = (all_dataframes[1].loc[(all_dataframes[1]['Matrix'] == ('RT' or 'Reference Toxicant')) & (all_dataframes[1]['Concentration'] == -88)])
+for item_number in (reftox.index):
+	human_error = 'A "Reference Toxicant" record in the Matrix field can not have a -88 in the Concentration field'
+	unique_error = '{ "column": "Concentration", "error_type": "Toxicity Error", "error": "%s" }' % (human_error)
+	dcAddErrorToList("custom_errors_reftox",int(reftox['tmp_row'].loc[item_number]),unique_error,all_dataframes[1])
+
+## SUMMARY RESULT TABLE
 
 # summary must not be a groupby otherwise below functions wont work
 # all_dataframes[1] is the toxicity results data
@@ -100,4 +134,25 @@ def getPctControl(row):
         return row
 
 summary = summary.apply(getPctControl, axis=1)
+
+def get_p_value(summary):
+	for index, values in summary['ToxBatch'].iteritems():
+		station_code = summary.ix[index, 'StationID']
+		cneg_result = summary[['Result']].where((summary['SampleTypeCode'] == 'CNEG') & (summary['ToxBatch'] == values))
+		result_both = summary[['Result']].where((summary['ToxBatch'] == values) & (summary['StationID'] == station_code) )
+		cneg_result = cneg_result.dropna()
+		result_both = result_both.dropna()
+		t, p = stats.ttest_ind(cneg_result, result_both, equal_var = False)
+		summary.ix[index, 't - stat'] = t
+		summary.ix[index, 'p - value'] = p/2 #we divide by 2 to make it a 1 tailed
+	if (t < 0):
+		summary.ix[index, 'Significance'] = 'NSC'
+	else:
+		if (p <= .05):
+			summary.ix[index, 'Significance'] = 'SC'
+		else:
+			summary.ix[index, 'Significance'] = 'NSC'
+get_p_value(summary)
+
+#summary.to_csv('test.csv', sep='\t', encoding='utf-8')
 print(summary)
